4cbf3987112baca7a4414c3574d74c65c33a01c2
yongfeng <yongfeng99@gmail.com>
2024-09-21 03:53:04 +0800

    chainlink vrf wip

--- contracts/Angpow.sol
+++ contracts/Angpow.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract AngpowContract is OwnableUpgradeable, ReentrancyGuardUpgradeable, AccessControlUpgradeable {
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+contract AngpowContract is ReentrancyGuardUpgradeable, AccessControlUpgradeable, VRFConsumerBaseV2Plus {
 
     struct Angpow {
         address donator;
@@ -24,17 +26,57 @@ contract AngpowContract is OwnableUpgradeable, ReentrancyGuardUpgradeable, Acces
     event AngpowReceived(address recipient, uint256 id, address token, uint256 token_amount, uint256 index);
 
 
+
+
+
+
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+
+    // Your subscription ID.
+    uint256 public s_subscriptionId;
+
+    // Past request IDs.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    bytes32 public keyHash = 0x1770bdc7eec7771f7ba4ffd640f34260d7f095b79c92d34a5b2551d6f6cfd2be;
+
+    uint32 public callbackGasLimit = 100000;
+
+    uint16 public requestConfirmations = 3;
+
+    uint32 public numWords = 2;
+
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+
+
+
+
+
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
+    constructor() VRFConsumerBaseV2Plus(0x5CE8D5A2BC84beb22a398CCA51996F7930313D61) {
         _disableInitializers();
     }
 
-    function initialize() public initializer {
+    function initialize(uint256 subscriptionId) public initializer {
+        
         __AccessControl_init();
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
+
+        s_subscriptionId = subscriptionId;
+
     }
 
     function createAngpow(uint256 _id, address _token, uint256 _tokenAmount, uint256 _quantity) external payable {
@@ -73,6 +115,61 @@ contract AngpowContract is OwnableUpgradeable, ReentrancyGuardUpgradeable, Acces
     function withdrawEth() external onlyRole(DEFAULT_ADMIN_ROLE) {
         (bool sent, ) = msg.sender.call{value: address(this).balance}("");
         require(sent, "Failed to send Ether.");
+    }
+
+
+
+
+
+
+    // Assumes the subscription is funded sufficiently.
+    // @param enableNativePayment: Set to `true` to enable payment in native tokens, or
+    // `false` to pay in LINK
+    function requestRandomWords(
+        bool enableNativePayment
+    ) external onlyOwner returns (uint256 requestId) {
+        // Will revert if subscription is not set and funded.
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: enableNativePayment
+                    })
+                )
+            })
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] calldata _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
     }
 
 }
